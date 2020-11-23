753a512d3705ff865d4466624129b236727c2b47
Soldy <4786022+Soldy@users.noreply.github.com>
2020-11-23 10:40:18 +0000

    test update
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test.js
+++ test.js
@@ -4,6 +4,9 @@ const test  = new (require('nanoTest')).test({
     'progressBar':'false',
     'debugPrint' : 'short'
 });
+const missingFunction = new (function(){})
+const errorFunction = function(){ return subidubibu();};
+console.log('the expectation is 4 ok 10 failed 1 error 1 missing');
 
 let nanoTestFull=()=>{
     let nanotest = require('./index.js').test;
@@ -25,7 +28,23 @@ let nanoTestFull=()=>{
     nanoTest.add('"12.", (()=>{return {0:1}})(), "j===", {0:1}',  ()=>{return {0:1};}, 'j===', {0:1});
     nanoTest.add('"13.", (()=>{return {0:1}})(), "j==", {0:1}',   ()=>{return {0:1};}, 'j==', {0:1});
     nanoTest.add('"14.", (()=>{return {0:1}})(), "!=", {0:1}',    ()=>{return {0:1};}, '!=', {0:1});
-    nanoTest.run({ok:4,failed:10});// this test is a test test so 4 ok  10 failed is the exectation
+    nanoTest.add(
+        '"15.", errorFunctior, "!=", {0:1}',
+        {
+            'function':errorFunction,
+            'options':[]
+        },
+        '!=', {0:1}
+    );
+    nanoTest.add(
+        '"16.", missingFunction, "!=", {0:1}',
+        {
+            'function':missingFunction.a,
+            'options':[]
+        },
+        '!=', {0:1}
+    );
+    nanoTest.run({ok:4,failed:10,error:1,missing:1});// this test is a test test so 4 ok  10 failed 1 error 1 missing is the exectation
 };
 
 nanoTestFull(); 
