e66750bd0d26c12391174390996ebd0d6c9c404e
Soldy <4786022+Soldy@users.noreply.github.com>
2020-11-23 09:24:28 +0000

    version update
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test.js
+++ test.js
@@ -1,9 +1,16 @@
 'use strict';
 
+const test  = new (require('nanoTest')).test({
+    'progressBar':'false',
+    'debugPrint' : 'short'
+});
 
 let nanoTestFull=()=>{
     let nanotest = require('./index.js').test;
-    let nanoTest = new nanotest();
+    let nanoTest = new nanotest({
+        'progressBar':'false',
+        'debugPrint':'short'
+    });
     nanoTest.add('"1. ", (()=>{return "0"})(), "!=", "1"',        ()=>{return '0';}, '!=', '1');
     nanoTest.add('"2. ", (()=>{return "0"})(), "!=", 1',          ()=>{return '0';}, '!=', 1);
     nanoTest.add('"3. ", (()=>{return "0"})(), "==", "0"',        ()=>{return '0';}, '==', '0');
@@ -18,6 +25,7 @@ let nanoTestFull=()=>{
     nanoTest.add('"12.", (()=>{return {0:1}})(), "j===", {0:1}',  ()=>{return {0:1};}, 'j===', {0:1});
     nanoTest.add('"13.", (()=>{return {0:1}})(), "j==", {0:1}',   ()=>{return {0:1};}, 'j==', {0:1});
     nanoTest.add('"14.", (()=>{return {0:1}})(), "!=", {0:1}',    ()=>{return {0:1};}, '!=', {0:1});
-    nanoTest.run();
+    nanoTest.run({ok:4,failed:10});// this test is a test test so 4 ok  10 failed is the exectation
 };
-nanoTestFull();
+
+nanoTestFull(); 
